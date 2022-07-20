035105281bdc32a4e2d92fee1964ed8ed787dbe8
Nadya Baranova <12nadya11@gmail.com>
2022-07-20 20:24:33 +0300

    add: add my desicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	// your solution
+
+	let fullStr = str + bracketsConfig.join().replace(/,/g, '');
+
+	const brackets = {
+		")": "(",
+		"]": "[",
+		"}": "{",
+
+	};
+
+	function isClosedBracket(item) {
+		return [")", "]", "}"].indexOf(item) > -1;
+	}
+
+
+	const stack = [];
+
+
+	for (let i = 0; i < fullStr.length; i++) {
+		if (isClosedBracket(fullStr[i])) {
+			if (brackets[fullStr[i]] !== stack.pop())
+				console.log(stack)
+		} else if (fullStr[i] === "|" || fullStr[i] === "0" || fullStr[i] === "1" || fullStr[i] === "2" ||
+			fullStr[i] === "3" || fullStr[i] === "4" || fullStr[i] === "5" || fullStr[i] === "6" ||
+			fullStr[i] === "7" || fullStr[i] === "8" || fullStr[i] === "9") {
+			if (fullStr[i] === stack[stack.length - 1]) {
+				stack.pop();
+			} else {
+				stack.push(fullStr[i]);
+				console.log(stack)
+			}
+		}
+		else {
+			stack.push(fullStr[i]);
+			console.log(stack)
+		}
+	}
+
+	if (stack.length === 0) {
+		return true;
+	} else {
+		return false
+	}
 }
