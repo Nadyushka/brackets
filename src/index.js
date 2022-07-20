d4e75516af8e622fa2efe7d958affe5d67b1e529
Nadya Baranova <12nadya11@gmail.com>
2022-07-20 21:42:18 +0300

    add: finish task

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
 	// your solution
 
-	let fullStr = str + bracketsConfig.join().replace(/,/g, '');
+	let fullStr = str;
 
 	const brackets = {
 		")": "(",
 		"]": "[",
 		"}": "{",
-
+		"2": "1",
+		"4": "3",
+		"6": "5"
 	};
 
 	function isClosedBracket(item) {
-		return [")", "]", "}"].indexOf(item) > -1;
+		return [")", "]", "}", "2", "4", "6"].indexOf(item) > -1;
 	}
 
 
@@ -25,17 +27,18 @@ module.exports = function check(str, bracketsConfig) {
 			} else {
 				return false;
 			}
-		} else if (fullStr[i] === "|" || fullStr[i] === "0" || fullStr[i] === "1" || fullStr[i] === "2" ||
-			fullStr[i] === "3" || fullStr[i] === "4" || fullStr[i] === "5" || fullStr[i] === "6" ||
-			fullStr[i] === "7" || fullStr[i] === "8" || fullStr[i] === "9") {
+		} else if (fullStr[i] === "|" || fullStr[i] === "7" || fullStr[i] === "8") {
 			if (fullStr[i] === stack[stack.length - 1]) {
 				stack.pop();
+				console.log(stack)
 			} else {
 				stack.push(fullStr[i]);
+				console.log(stack)
 			}
 		}
 		else {
 			stack.push(fullStr[i]);
+			console.log(stack)
 		}
 	}
 
